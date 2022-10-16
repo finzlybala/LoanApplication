5260caf6c7d4f10c4b5d19f731565c0123e8caab
finzlybala <109542593+finzlybala@users.noreply.github.com>
2022-10-16 19:50:11 +0530

    updated

--- LoanApplication/src/main/java/com/example/LoanApplication/controller/Controller.java
+++ LoanApplication/src/main/java/com/example/LoanApplication/controller/Controller.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,4 +43,9 @@ public class Controller {
      public Object print(@PathVariable int id) {
 		return service.getLoansByCustomerId(id);
 		}
+	@PutMapping("customer/payment/{id}")
+    public Object payment(@PathVariable int id) {
+         return service.PaymentChanger(id);
+           }
+ 
 }
--- LoanApplication/src/main/java/com/example/LoanApplication/service/LoanService.java
+++ LoanApplication/src/main/java/com/example/LoanApplication/service/LoanService.java
@@ -2,6 +2,7 @@ package com.example.LoanApplication.service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import com.example.LoanApplication.Dao.LoanDao;
 import com.example.LoanApplication.Dao.PaymentscheduleDao;
@@ -21,6 +22,7 @@ public class LoanService {
 	private PaymentscheduleDao schedules;
 
 	public String applyLoan(Loan loan) {
+		LocalDate endDate = loan.getLoanStartDate().plusMonths(loan.getMaturityDate());
 		List<Paymentschedule> schedules = loan.getSchedule();
 		int paymentSchedule = loan.getMaturityDate() / loan.getPaymentFrequency();
 		int years = loan.getMaturityDate() / 12;
@@ -34,30 +36,28 @@ public class LoanService {
 			detials.setPaymentDate(tempPaymentDate);
 			if (loan.getPaymentTerm().equalsIgnoreCase("Even Principal")) {
 				detials.setPrincipalamount(loan.getLoanAmount() / paymentSchedule);
-				detials.setProjectedIntrest(tempLoanAmount / loan.getIntrestRate());
+				detials.setProjectedIntrest((tempLoanAmount * loan.getIntrestRate()) / 100);
 				tempLoanAmount = tempLoanAmount - detials.getPrincipalamount();
 				detials.setPaymentAmount(detials.getProjectedIntrest() + detials.getPrincipalamount());
 			} else {
-				if(detials.getPaymentDate().equals(loan.getMaturityDate())) {
+				if (detials.getPaymentDate().equals(endDate)) {
 					detials.setPrincipalamount(loan.getLoanAmount());
-					
-				}
-				else {
+				} else {
 					detials.setPrincipalamount(0);
 				}
-				
-				detials.setProjectedIntrest((loan.getLoanAmount() / loan.getIntrestRate()) / paymentSchedule * years);
-				if (detials.getPaymentDate().equals(loan.getMaturityDate())) {
+				detials.setProjectedIntrest(((loan.getLoanAmount() * loan.getIntrestRate()) / 100) / 12);
+				if (detials.getPaymentDate().equals(endDate)) {
 					paymentAmount = detials.getProjectedIntrest() + loan.getLoanAmount();
-					
 				} else {
 					paymentAmount = detials.getProjectedIntrest();
 				}
 				detials.setPaymentAmount(paymentAmount);
 			}
-			if (detials.getPaymentDate().equals(LocalDate.now())) {
-				detials.setPaymentStatus(" AwaitingPayment");
-			} else if (detials.getPaymentDate().compareTo(LocalDate.now()) < 0) {
+			if (detials.getPaymentDate().equals(LocalDate.now())
+					&& !detials.getPaymentStatus().equalsIgnoreCase("Paid")) {
+				detials.setPaymentStatus("AwaitingPayment");
+			} else if (detials.getPaymentDate().compareTo(LocalDate.now()) < 0
+					|| detials.getPaymentStatus().equalsIgnoreCase("AwaitingPayment")) {
 				detials.setPaymentStatus("Paid");
 			}
 			schedules.add(detials);
@@ -69,21 +69,24 @@ public class LoanService {
 
 	public List<Loan> getAllDetails() {
 		return loans.findAll();
-
 	}
 
 	public List<Paymentschedule> getLoansByCustomerId(int id) {
-	Loan detials=loans.findById(id).get();
-	for (Paymentschedule schedule :detials.getSchedule()) {
-		if (schedule.getPaymentDate().equals(LocalDate.now())) {
-			schedule.setPaymentStatus("AwaitingPayment");
+		Loan detials = loans.findById(id).get();
+		for (Paymentschedule schedule : detials.getSchedule()) {
+			if (schedule.getPaymentDate().equals(LocalDate.now())
+					&& !schedule.getPaymentStatus().equalsIgnoreCase("Paid")) {
+				schedule.setPaymentStatus("AwaitingPayment");
+			}
 		}
-		else if (schedule.getPaymentDate().compareTo(LocalDate.now())<0) {
-			schedule.setPaymentStatus("Paid");
-		}
-		schedules.save(schedule);
-	} 
+		loans.save(detials);
 		return detials.getSchedule();
 	}
 
+	public Paymentschedule PaymentChanger(int id) {
+		Paymentschedule detials = schedules.findById(id).get();
+		detials.setPaymentStatus("Paid");
+
+		return schedules.save(detials);
+	}
 }
